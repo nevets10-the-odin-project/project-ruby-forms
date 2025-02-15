9f72a458353c2c8b5654a7d2862d346b9e48c1e6
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-14 19:15:21 -0800

    Populated #create, add user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    # @user = User.new(params[:user])
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: %i[email username password])
   end
 end
